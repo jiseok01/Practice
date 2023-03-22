e3c4ef54f18c31809c2bd778b91bdcf37e7191d9
지석 정 <jjc820@naver.com>
2023-03-22 19:46:50 +0900

    두번쨰

--- code/소스.cpp
+++ code/소스.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main() {
 
 	int a = 0, b = 0, c = 0;
-	cout << a << b << c;
+	cout << a << b << c << endl;
 	cin >> a;
-	cout << a;
-	
+	cout << a << endl;
+	cin >> b; 
+	cout << b;
 }
