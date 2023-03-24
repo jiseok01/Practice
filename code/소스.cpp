71ed6a0f3f6ba33fce131bc531950de8b3632870
jiseok01 <jjc820@naver.com>
2023-03-24 17:32:03 +0900

    c++과제

--- code/소스.cpp
+++ code/소스.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
-
 using namespace std;
 
+class ThreeMatrices {
+	int a[3][5] = { {5, 10, 2, 7, 5}, {4, 6, 2, 2, 9}, {1, 9, 2, 8, 4} };
+	int b[3][5] = { {5, 2, 7, 4, 5}, {10, 6, 9, 2, 3}, {2, 6, 4, 7, 1} };
+	int c[3][5];
+public:
+	ThreeMatrices();
+	void printC();
+	void biggerC();
+	void smallerC();
+};
+ThreeMatrices::ThreeMatrices() { fill_n(c[0], 15, 0); }
+void ThreeMatrices::printC() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 5; j++)	
+			cout << c[i][j] << ' ';
+		cout << endl;
+	}
+}
+void ThreeMatrices::biggerC() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (a[i][j] > b[i][j])
+				c[i][j] = a[i][j];
+			else
+				c[i][j] = b[i][j];
+		}
+	}
+}
+void ThreeMatrices::smallerC() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (a[i][j] < b[i][j])
+				c[i][j] = a[i][j];
+			else
+				c[i][j] = b[i][j];
+		}
+	}
+}
 int main() {
-
-	int a = 0, b = 0, c = 0;
-	cout << a << b << c << endl;
-	cin >> a;
-	cout << a << endl;
-	cin >> b; 
-	cout << b;
+	ThreeMatrices m; 
+	cout << "initial..." << endl;  m.printC();
+	cout << "bigger..."  << endl;  m.biggerC();  m.printC();
+	cout << "smaller..." << endl;  m.smallerC(); m.printC();
 }
