7589257349445847ff6fe6d82596a6fe1efe8519
jiseok01 <jjc820@naver.com>
2023-03-27 22:39:23 +0900

    연습

--- code/소스.cpp
+++ code/소스.cpp
@@ -1,11 +1,18 @@
-#include "Circle.h"
-#include <iostream> 
+#include <iostream>
 using namespace std;
-
+void print_array(int arr[][5]) {    //  (*arr)[5] 과동일
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 5; j++) 
+			cout<< arr[i][j] << ' ';cout<< endl;
+	}
+	cout<< endl;
+}
 int main() {
-	Circle donut;
-	cout<< "donut 면적은" << donut.getArea() << endl;
-	Circle pizza;
-	pizza.radius= 10; 
-	cout<< "pizza 면적은" << pizza.getArea() << endl;
+	int a[3][5] = { 7 };    
+	print_array(a);int b[3][5];  
+	fill(b[0], b[3], 7);  
+	print_array(b);int c[3][5];  
+	fill_n(c[0], 15, 7);  
+	print_array(c);
+	return 0;
 }
