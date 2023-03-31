306da2668a3d41bfad50f355c77f08f8510209e7
jiseok01 <jjc820@naver.com>
2023-03-31 17:08:01 +0900

    과제 9번

---
+++ code/과제9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Line {
+public:	
+	int sx, sy;
+	int ex, ey;
+	void setTwoPoints() {
+		cout << "시작점 좌표 정수 두 개를 입력하세요.";
+		cin >> sx >> sy;
+		cout << "끝점 좌표 정수 두 개를 입력하세요.";
+		cin >> ex >> ey;
+	}
+	double getLineLength() {
+		return sqrt(pow(ex - sx, 2) + pow(ey - sy, 2));
+	}
+};
+
+int main() {
+	Line myline;
+	myline.setTwoPoints();
+	cout << "myline의 길이는 " << myline.getLineLength() << " 입니다. \n";
+}
