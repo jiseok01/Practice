d4ed88707f8cc25ed6272aad6d0f22de32a096f5
jiseok01 <126743121+jiseok01@users.noreply.github.com>
2023-04-10 22:05:18 +0900

    Add files via upload

---
+++ py/2024168-PythonCode3.py
@@ -0,0 +1,27 @@
+import turtle
+import random
+angle,dist,curx,curY=[0]*4
+t1 = turtle.Turtle()
+t1.shape('turtle')
+t1.penup()
+t2 = turtle.Turtle()
+t2.shape('turtle')
+t2.penup()
+t3 = turtle.Turtle(0
+t3.shape('turtle')
+t3.penup()
+for i in range(0,100)
+    t1,t2,t3.angle=random.randrange(0,360)
+    t1,t2,t3.dist=random.randrange(1,100)
+    t1,t2,t3..left(angle)
+    t1,t2,t3..forward(dist)
+    curX=turtle.xcor()
+    curY=turtle.ycor()
+                   
+    if(-swidth/2<=curx and curx <=swidth/2) and (-sheight/2 <=curY and curY <=
+    sheight/2):
+        pass
+    else :
+        turtle.goto(0,0)
+      
+turtle.done()
